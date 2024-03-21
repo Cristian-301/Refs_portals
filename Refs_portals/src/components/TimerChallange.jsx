43718297660762aa4502afcfef17fb1f8e092f28
Cristian <cristianmihai193@gmail.com>
2024-03-21 12:31:26 +0100

    modal

--- Refs_portals/src/components/TimerChallange.jsx
+++ Refs_portals/src/components/TimerChallange.jsx
@@ -1,7 +1,9 @@
 import { useState, useRef } from "react"
+import ResultModal from "./ResutlModal.jsx"
 
 export default function TimerChallange({title, targetTime}) {
   const timer = useRef()
+  const dialog = useRef()
 
   const [timerStarted, setTimerStarted] = useState(false)
   const [timerExpired, setTimerExpired] = useState(false)
@@ -9,6 +11,7 @@ export default function TimerChallange({title, targetTime}) {
   function handleClick() {
     timer.current = setTimeout(() => {
       setTimerExpired(true)
+      dialog.current.showModal()
     }, targetTime * 1000)
 
     setTimerStarted(true)
@@ -21,9 +24,11 @@ export default function TimerChallange({title, targetTime}) {
 
 
   return(
+    <>
+    < ResultModal ref={dialog} targetTime={targetTime} result='lost'/>
+    
     <section className="challenge">
       <h2>{title}</h2>
-      {timerExpired && <p> You lost </p> }
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
@@ -36,5 +41,6 @@ export default function TimerChallange({title, targetTime}) {
        {timerStarted ? 'Time is running...' : 'Timer inactive'}  
       </p>
     </section>
+    </>
   )
 }
